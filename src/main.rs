08bd4ea4ac217a367e210dbec974675b43014cd1
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-09 00:00:43 +0530

    refactor: extract methods to modules

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
+mod rat;
+use crate::rat::{print_file, read_file};
 use anyhow::{Context, Result};
 use clap::Parser;
 use std::fs::File;
-use std::io::{self, Write};
-use std::io::{BufRead, BufReader};
+use std::io::BufReader;
 
 #[derive(Parser)]
 struct CommandArgs {
     path: std::path::PathBuf,
-}
-
-fn read_file(path: &std::path::PathBuf) -> Result<std::io::BufReader<File>> {
-    let file = File::open(path).with_context(|| format!("Failed to open file {:?}", path))?;
-    return Ok(BufReader::new(file));
-}
-
-fn print_file(reader: std::io::BufReader<File>) -> Result<()> {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-
-    for line in reader.lines() {
-        let line = line.with_context(|| format!("Failed to read line"))?;
-        writeln!(handle, "{}", line)?;
-    }
-    Ok(())
 }
 
 fn main() -> Result<()> {
