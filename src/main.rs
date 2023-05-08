27f45d48d04a6cd8b9e509dd9b1558d2504903f1
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-08 22:27:40 +0530

    feat: add better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -7,12 +8,19 @@ struct CommandArgs {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = CommandArgs::parse();
-    let file = File::open(args.path).unwrap();
+fn read_file(path: &std::path::PathBuf) -> Result<()> {
+    let file = File::open(path).with_context(|| format!("Failed to open file {:?}", path))?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        println!("{}", line.unwrap());
+        println!("{}", line.with_context(|| "Failed to read line")?);
     }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = CommandArgs::parse();
+
+    read_file(&args.path).with_context(|| format!("Failed to read file {:?}", args.path))?;
+    Ok(())
 }
